0c676e8dbd0cebbafedba419b940b14447aaf33f
Soren Wasmer <sorenmwasmer@gmail.com>
2025-04-02 16:34:32 -0500

    Remove outdated animations directory

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import "./fonts/fonts.css"; // ✅ THIS is where the import belongs
+import "./fonts/fonts.css";
 
 import "./index.css";
 
